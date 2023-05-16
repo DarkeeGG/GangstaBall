aa6a5b3f6e621638127d0a67564f06f380a27c89
Zosia Samosia <Mlody@G.pl>
2023-05-16 14:09:03 +0200

    gangsta ball

---
+++ js/scrpits.js
@@ -0,0 +1,15 @@
+'use strict'
+const ball = document.querySelector('#ball')
+
+document.addEventListener('click',(e) =>{
+
+    console.log(e)
+
+console.log(e.clientX);
+console.log(e.clientY);
+
+
+ ball.style.transform = `translateY(${e.clientY-25}px)`
+ ball.style.transform += `translateX(${e.clientX-25}px)`
+
+});
